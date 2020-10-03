496f65aec10dfd5bfb692e87281d4a52c22b5786
hiiri <alcohen@student.hive.fi>
2020-10-03 19:44:16 +0300

    Fix grammar

--- src/values.js
+++ src/values.js
@@ -2,5 +2,5 @@ import { constants } from "http2"
 
 export const BOT_OAUTH_TOKEN = "xoxb-1392046829411-1392022056962-pYaBZ2miIn9g6dWbrzZiiGDo"
 export const BOT_CHANNEL = '#bot-channel'
-export const INTRO_MESSAGE = "Hello, I am here to help maintain your connections to your fellow Hivers!\nJust react with a Bee and you will be given a random pair every monday at 8:42!\nSo lets Bee Friends and support eachother :)"
+export const INTRO_MESSAGE = "Hello, I am here to help maintain your connections to your fellow Hivers!\nJust react with a Bee and you will be given a random pair every Monday at 8:42!\nSo let's Bee Friends and support each other :)"
 export const GROUP_SIZE = 2
